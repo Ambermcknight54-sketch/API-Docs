aee0d0915a62c31c36764715815a6281bd8815a6
Amber <ambermcknight54@yahoo.com>
2026-05-21 14:31:14 -0500

    added a curly brace

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ async function getName(customName) {
 
 console.log("Hi World!");
 getName("Pita");
- 
+
 async function getName(customName) {
   const data= {name: "customName"}; 
   const dataString= name URLSearchParams(data);
@@ -100,4 +100,18 @@ async function getName(customName) {
   outputTag.innerText= customName + "is 100% male."; 
 
 }}
+
+console.log("Hi World!");
+getName("Kim");
+ 
+async function getName(customName) {
+  const data= {name: "customName"}; 
+  const dataString= name URLSearchParams(data);
+  const response= await fetch(" https://api.generize.io" + "?"+ "datastring"); 
+  const result= await response.json();
+  const percent=probability * 100;
+  const gender=result.gender;
+  const outputTag=document.querySelector("output");
+  outputTag.innerText= customName + "is" + percent + "%" + gender +  "."; 
+}
 
