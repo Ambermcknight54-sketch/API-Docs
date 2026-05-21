e3aa92c2af1e91c866c7bdeefc99517dfed59a78
Amber <ambermcknight54@yahoo.com>
2026-05-21 12:45:16 -0500

    added data in  script

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log(!"Hi World!");
+
+async function getName() {
+  const data= {name: "Pita"}; 
+  const datastring= name URLSearchParams(data);
+  const response= await fetch(" https://api.generize.io");   
+}
+   
+
+fetch(" https://api.generize.io?namePita")
