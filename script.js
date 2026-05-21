46c520e87ddb42060659aee2434e04ad266d2665
Amber <ambermcknight54@yahoo.com>
2026-05-21 13:02:00 -0500

    using script to get names from the api

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-console.log(!"Hi World!");
+console.log("Hi World!");
 
 async function getName() {
+  debugger;
+  console.log (aaaaaaaaa)
   const data= {name: "Pita"}; 
   const datastring= name URLSearchParams(data);
-  const response= await fetch(" https://api.generize.io");   
+  const response= await fetch(" https://api.generize.io"); 
+  const result= await response.json();
 }
-   
+   getName();
+
+
+  console.log("Hi World!");
+
+async function getName(customName) {
+  debugger;
+  console.log (aaaaaaaaa)
+  const data= {name: "customName"}; 
+  const datastring= name URLSearchParams(data);
+  const response= await fetch(" https://api.generize.io"); 
+  const result= await response.json(;)
+}
+   getName("Pita"); 
 
 fetch(" https://api.generize.io?namePita")
